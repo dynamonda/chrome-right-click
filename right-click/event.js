70befd937ea1f0359040854a08208c2b77fb1c02
dynamonda <v7gj9kk@gmail.com>
2021-09-24 07:56:39 +0900

    メニューをクリックするとダイアログが表示されるようにする

--- right-click/event.js
+++ right-click/event.js
@@ -16,26 +16,10 @@
             parentId: 'parent',
             title: '子メニュー2'
         });
-        chrome.contextMenus.create({
-            id: 'child3',
-            parentId: 'parent',
-            title: '子メニュー3'
-        });
-        chrome.contextMenus.create({
-            id: 'grandchild1',
-            parentId: 'child1',
-            title: '孫メニュー1'
-        });
-        chrome.contextMenus.create({
-            id: 'grandchild2',
-            parentId: 'child1',
-            title: '孫メニュー2'
-        });
     });
 
     // メニューをクリック時に実行
     chrome.contextMenus.onClicked.addListener(item => {
-        console.log(item);
-        console.log(item.menuItemId);
+        alert("ID=" + item.menuItemId);
     });
 }
