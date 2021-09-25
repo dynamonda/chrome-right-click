e24ad18ca6eb8d09f19dcf2a6063b49018a9885a
dynamonda <v7gj9kk@gmail.com>
2021-09-25 13:04:48 +0900

    動画IDとチャンネルIDで開けるように実装

--- right-click/event.js
+++ right-click/event.js
@@ -3,9 +3,17 @@
 {
     // メニューを登録
     chrome.runtime.onInstalled.addListener(function(){
+        // 動画
         const menuItem = chrome.contextMenus.create({
             id: 'youtube_id',
-            title: "テスト「%s」",
+            title: "YouTubeをID '%s' で開く",
+            contexts: ['selection']
+        });
+
+        // チャンネル
+        const channel = chrome.contextMenus.create({
+            id: 'youtube_channel',
+            title: "YouTubeをチャンネル '%s' で開く",
             contexts: ['selection']
         });
     });
@@ -17,7 +25,11 @@
 
         if(selectedMenu === "youtube_id"){
             chrome.tabs.create({
-                url: 'https://google.com'
+                url: 'https://youtube.com/watch?v=' + selectedText
+            });
+        }else if(selectedMenu === "youtube_channel"){
+            chrome.tabs.create({
+                url: 'https://youtube.com/channel/' + selectedText
             });
         }else{
             alert("selected=" + selectedText + ", menu=" + selectedMenu);
